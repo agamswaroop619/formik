a035d738b63262a888351b8fada6ce285e359832
agamswaroop619 <agam.swaroop619@gmail.com>
2024-04-05 11:28:23 +0530

    Testing using formik hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
-
+import { useFormik } from "formik";
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+    },
+  });
+  console.log(formik);
   return (
     <div className="App">
       <form>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          type="text"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+        />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
