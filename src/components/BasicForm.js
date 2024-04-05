f8297314dfca865208f1f2ecb2852bf439cd48ef
agamswaroop619 <agam.swaroop619@gmail.com>
2024-04-05 12:02:07 +0530

    add: New Input fields

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -5,6 +5,9 @@ export default function Basicform() {
   const formik = useFormik({
     initialValues: {
       name: "",
+      email: "",
+      password: "",
+      confirmpassword: "",
     },
   });
   console.log(formik);
@@ -16,6 +19,27 @@ export default function Basicform() {
           id="name"
           type="text"
           value={formik.values.name}
+          onChange={formik.handleChange}
+        />
+        <label htmlFor="email">E-Mail:</label>
+        <input
+          id="email"
+          type="text"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
+        <label htmlFor="name">Password:</label>
+        <input
+          id="password"
+          type="text"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        <label htmlFor="name">Confirm Password:</label>
+        <input
+          id="confirmpassword"
+          type="text"
+          value={formik.values.confirmpassword}
           onChange={formik.handleChange}
         />
         <button type="submit">Submit</button>
